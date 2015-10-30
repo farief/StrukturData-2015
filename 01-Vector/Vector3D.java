7c0c9011345e7a82ee37e3da46136f04738a19b6
fadel <fadelariefa02@gmail.com>
2015-10-30 17:24:41 +0700

    no message

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -1,11 +1,27 @@
+//unit tes 1
 public class Vector3D {
-    double x,y,z;
+    private double x;
+    private double y ;
+    private double z;
     
     public Vector3D (double x, double y, double z){
     this.x=x;
     this.y=y;
-    this.z=z; 
-}
+    this.z=z;
+ }
+ 
+ 
+  public double getX(){
+      return x;
+    }
+      
+  public double getY(){
+      return y;
+    }
+    
+    public double getZ(){
+        return z;
+    }       
 
 }
 
