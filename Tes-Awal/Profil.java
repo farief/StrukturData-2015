3f3feb5c37cb9fb879ce3318de3a534c86b7b253
fadel <fadelariefa02@gmail.com>
2015-11-06 14:53:19 +0700

    TesAwal

---
+++ Tes-Awal/Profil.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Profil here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Profil
+{
+   private String nama ="Fadel Ariefa";
+   private String nim ="1408107010004";
+   
+   public Profil(){
+       
+    }
+    
+    public Profil(String nama, String nim){
+        this.nama= nama;
+        this.nim= nim;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public String getNim(){
+        return nim;
+        
+    }
+}
+         
+   
+   
+       
+     
+
